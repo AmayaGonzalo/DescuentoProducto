d14869e13cb9edbadea0b177de069b6b3bd6a59b
Amaya Gonzalo Javier <103613393+AmayaGonzalo@users.noreply.github.com>
2022-04-30 23:44:30 -0300

    Initial commit

---
+++ src/index.ts
@@ -0,0 +1,5 @@
+let precioProducto : number = 450.50;
+let porcentajeDescuento : number = 0.1;
+let precioFinal : number = precioProducto * porcentajeDescuento;
+
+console.log ("El precio final es: "precioFinal);
